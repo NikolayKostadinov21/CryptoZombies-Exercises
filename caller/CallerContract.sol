f3de297d476b1922afc8ed4f062409310db7f76d
Nikolay Kostadinov <118672723+NikolayKostadinov21@users.noreply.github.com>
2023-02-16 09:33:57 +0200

    Update CallerContract.sol

--- caller/CallerContract.sol
+++ caller/CallerContract.sol
@@ -1,10 +1,13 @@
 pragma solidity 0.5.0;
 import "./EthPriceOracleInterface.sol";
-contract CallerContract {
-  EthPriceOracleInterface private oracleInstance;
-  address private oracleAddress;
-  function setOracleInstanceAddress (address _oracleInstanceAddress) public {
-    oracleAddress = _oracleInstanceAddress;
-    oracleInstance = EthPriceOracleInterface(oracleAddress);
-  }
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+contract CallerContract is Ownable{
+    EthPriceOracleInterface private oracleInstance;
+    address private oracleAddress;
+    event newOracleAddressEvent(address oracleAddress);
+    function setOracleInstanceAddress (address _oracleInstanceAddress) public onlyOwner {
+      oracleAddress = _oracleInstanceAddress;
+      oracleInstance = EthPriceOracleInterface(oracleAddress);
+      emit newOracleAddressEvent(oracleAddress);
+    }
 }
