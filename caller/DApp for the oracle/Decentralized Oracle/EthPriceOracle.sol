ca4738acaffe132439e7ea3e2256777ea4d3e355
Nikolay Kostadinov <118672723+NikolayKostadinov21@users.noreply.github.com>
2023-02-16 15:13:57 +0200

    Update EthPriceOracle.sol

--- caller/DApp for the oracle/Decentralized Oracle/EthPriceOracle.sol
+++ caller/DApp for the oracle/Decentralized Oracle/EthPriceOracle.sol
@@ -1,10 +1,12 @@
 pragma solidity 0.5.0;
 import "openzeppelin-solidity/contracts/access/Roles.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./CallerContractInterface.sol";
 contract EthPriceOracle {
   using Roles for Roles.Role;
   Roles.Role private owners;
   Roles.Role private oracles;
+  using SafeMath for uint256;
   uint private randNonce = 0;
   uint private modulus = 1000;
   uint private numOracles = 0;
@@ -59,15 +61,16 @@ contract EthPriceOracle {
     uint numResponses = requestIdToResponse[_id].length;
     if (numResponses == THRESHOLD) {
       uint computedEthPrice = 0;
-      for (uint f=0; f < requestIdToResponse[_id].length; f++) {
-        computedEthPrice += requestIdToResponse[_id][f].ethPrice;
+        for (uint f=0; f < requestIdToResponse[_id].length; f++) {
+        computedEthPrice = computedEthPrice.add(requestIdToResponse[_id][f].ethPrice);
       }
-      computedEthPrice = computedEthPrice / numResponses;
+      computedEthPrice = computedEthPrice.div(numResponses);
       delete pendingRequests[_id];
+      delete requestIdToResponse[_id];
       CallerContractInterface callerContractInstance;
       callerContractInstance = CallerContractInterface(_callerAddress);
-      callerContractInstance.callback(_ethPrice, _id);
-      emit SetLatestEthPriceEvent(_ethPrice, _callerAddress);
+      callerContractInstance.callback(computedEthPrice, _id);
+      emit SetLatestEthPriceEvent(computedEthPrice, _callerAddress);
     }
   }
 }
