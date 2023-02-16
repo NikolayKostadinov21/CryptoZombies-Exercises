11dbf7793138e71524a60373f3a294892dfe87bf
Nikolay Kostadinov <118672723+NikolayKostadinov21@users.noreply.github.com>
2023-02-16 12:16:14 +0200

    Update EthPriceOracle.js

--- caller/DApp for the oracle/EthPriceOracle.js
+++ caller/DApp for the oracle/EthPriceOracle.js
@@ -31,8 +31,26 @@ async function filterEvents (oracleContract, web3js) {
   })
 }
 
-async function addRequestToQueue(event) {
-    const callerAddress = event.returnValues.callerAddress
-    const id = event.returnValues.id
-    pendingRequests.push({callerAddress, id})
+async function addRequestToQueue (event) {
+  const callerAddress = event.returnValues.callerAddress
+  const id = event.returnValues.id
+  pendingRequests.push({ callerAddress, id })
+}
+
+async function processQueue (oracleContract, ownerAddress) {
+  let processedRequests = 0
+  while (pendingRequests.length > 0 && processedRequests < CHUNK_SIZE) {
+    const req = pendingRequests.shift()
+    await processRequest(oracleContract, ownerAddress, req.id, req.callerAddress)
+    processedRequests++
+  }
+}
+
+async function processRequest (oracleContract, ownerAddress, id, callerAddress) {
+    let retries = 0
+    while (retries < MAX_RETRIES) {
+        try {
+        } catch (error) {
+        }
+    }
 }
